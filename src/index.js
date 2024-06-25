5e85b4b9f3609713a71a9a54a401a7bc14aaee3c
Andres Munoz <andrefe91@gmail.com>
2024-06-24 22:06:06 -0500

    Basic webpack setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log("We appear to be running in a development environment");
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: "",
+        filename: "index.html",
+        scriptLoading: "defer",
+        template: "./src/index.html",
+        }),
+    ],
+};
+
+module.exports = config;
